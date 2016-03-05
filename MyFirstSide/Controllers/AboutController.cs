6b30e3ac1208fe200875a9a6a77445b4446b35d8
Amorea <amorea.cat@yandex.ru>
2016-03-05 05:56:16 +0300

    First Side

---
+++ MyFirstSide/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstSide.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Like()
+        {
+            return View();
+        }
+        public ActionResult Want()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstSide/Models/BooksFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstSide.Models
+{
+    public class BooksFactory
+    {
+        public static IEnumerable<Book> CreateBooks()
+        {
+            List<Book> books = new List<Book>();
+            books.Add(new Book()
+            {
+                Author = "Э. Юдковский",
+                Name = "\"Тройной контакт\""
+
+            });
+            books.Add(new Book()
+            {
+                Author = "А. П. Чехов",
+                Name = "\"Вишневый сад\""
+
+            });
+            books.Add(new Book()
+            {
+                Author = "Дж. К. Роулинг",
+                Name = "\"Гарри Поттер\""
+
+            });
+            return books;
+
+        }
+    }
+}
